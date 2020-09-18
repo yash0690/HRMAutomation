4d2bc1b51b864f9c25e5e3135bfcefbb6b8167b9
yasasviuf35@mail.fresnostate.edu <yasasviuf35@mail.fresnostate.edu>
2020-09-17 19:24:09 -0700

    assertion for test_addnewuser.py

--- PageObjects/AddUserPage.py
+++ PageObjects/AddUserPage.py
@@ -9,6 +9,10 @@ class AddUserPage:
     textbox_employeepassword_xpath = "//input[@id='password']"
     textbox_employeeconfirmpassword_xpath = "//input[@id='confirmpassword']"
     button_save_xpath = "//a[@id='systemUserSaveBtn']"
+    button_filter_xpath = "//i[contains(text(),'ohrm_filter')]"
+    textbox_employeeusernamesearch_xpath = "//input[@id='systemuser_uname_filter']"
+    button_search_xpath = "//a[@class='modal-action modal-close waves-effect btn primary-btn']"
+
 
 
 
@@ -44,6 +48,21 @@ class AddUserPage:
 
     def clickSave(self):
         self.driver.find_element_by_xpath(self.button_save_xpath).click()
+
+    def clickfilter(self):
+        self.driver.find_element_by_xpath(self.button_filter_xpath).click()
+
+    def setEmployeeusernamesearch(self,searchusername):
+        self.driver.find_element_by_xpath(self.textbox_employeeusernamesearch_xpath).clear()
+        self.driver.find_element_by_xpath(self.textbox_employeeusernamesearch_xpath).send_keys(searchusername)
+
+    def clicksearch(self):
+        self.driver.find_element_by_xpath(self.button_search_xpath).click()
+
+
+
+
+
 
 
 
--- testCases/test_addnewuser.py
+++ testCases/test_addnewuser.py
@@ -1,7 +1,9 @@
-#log into website with username and password and click on login
-#on left hand side Menu - click on Admin > User management > users
-#on right hand side click on +
-#Enter all the new user details and click on SAVE
+# log into website with username and password and click on login
+# on left hand side Menu - click on Admin > User management > users
+# on right hand side click on +
+# Enter all the new user details and click on SAVE
+import random
+import string
 import time
 
 from selenium.webdriver.support.select import Select
@@ -11,11 +13,12 @@ from PageObjects.AdminMenuPage import AdminMenu
 from PageObjects.LoginPage import LoginPage
 from utilities.readProperties import ReadConfig
 from utilities.customLogger import LogGen
-#NamingConvention: Test_ID_nameoftestcase
+
+
+# NamingConvention: Test_ID_nameoftestcase
 
 
 class Test_002_Addnewuser():
-
     baseURL = ReadConfig.getApplicationURL()
     username = ReadConfig.getUseremail()
     password = ReadConfig.getpassword()
@@ -38,20 +41,43 @@ class Test_002_Addnewuser():
         self.am.clickUsers()
         time.sleep(10)
 
-        #creating an object for AddUserPage
+        # creating an object for AddUserPage
 
         self.au = AddUserPage(self.driver)
         self.au.clickAdd()
         time.sleep(5)
         self.au.setEmployeeName("Adella Lopez")
         time.sleep(5)
-        self.au.setEmployeeUserName("hvhsjdb")
-        #time.sleep(3)
-        #dropdown = Select(self.au.setEssRoledropdown())
-        #time.sleep(3)
-        #dropdown.select_by_visible_text("Default ESS")  # by option what you are seeing on screen
+        randusername = random_generator()
+        print(randusername)
+        self.au.setEmployeeUserName(randusername)
+        # time.sleep(3)
+        # dropdown = Select(self.au.setEssRoledropdown())
+        # time.sleep(3)
+        # dropdown.select_by_visible_text("Default ESS")  # by option what you are seeing on screen
         self.au.setEmployeePassword("ABCabc123$")
         self.au.setEmployeeConfirmPassword("ABCabc123$")
         self.au.clickSave()
+        time.sleep(5)
+        self.au.clickfilter()
+        time.sleep(2)
+        self.au.setEmployeeusernamesearch(randusername)
+        self.au.clicksearch()
+        time.sleep(3)
+        self.usernamexxx = self.driver.find_element_by_xpath("//tr[1]//td[2]/ng-include").text
+        print(self.usernamexxx)
+
+        if randusername == self.usernamexxx:
+            assert True
+            self.logger.info("********* Add New User Test Passed *********")
+        else:
+            self.driver.save_screenshot(".\\Screenshots\\" + "test_adduser_scr.png")  # Screenshot
+            self.logger.error("********* Add New User Test Failed ************")
+            assert False
+
+        self.driver.close()
+        self.logger.info("******* Ending Add New User test **********")
 
 
+def random_generator(size=8, chars=string.ascii_lowercase):
+    return ''.join(random.choice(chars) for x in range(size))
